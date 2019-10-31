e74f13329899703ef5cf47de61e2a263dc2ebc39
lbz007 <49712344+lbz007@users.noreply.github.com>
2019-10-31 16:23:10 +0800

    Add files via upload

---
+++ src/global.h
@@ -0,0 +1,61 @@
+#ifndef _GLOBAL_
+#define _GLOBAL_ 
+#include <string>
+#include <vector>
+#define PRECISION 0.5 //精度 
+#define PI 3.141592653589793
+using namespace std;
+
+//结构体 
+struct pos{double x,y;}; //坐标 
+struct dpos{pos p;pos v;};//带点行进方向的点 方向表示为单位向量 
+struct line{int s,e;};   //边 
+//系统全局变量 
+extern string rootDir; //exe文件所在根目录
+extern string mapFile; //输入的文件名 
+extern string pureName; //输入的文件名 不带后缀
+//算法数据初始化全局变量 
+extern int Node_num;       	//多边形节点数
+extern int recNode_num;
+extern vector<pos> NodeList;      	//多边形节点坐标
+extern int Edge_num;       	//多边形边数
+extern vector<line> total_LineList;   //总多边形边链表
+extern line start_Edge;        //起始边
+extern line end_Edge;          //终止边
+extern int LineList1_num;      //第一组边数目 
+extern vector<line> LineList1;        //第一组边 
+extern int LineList2_num;      //第二组边数目 
+extern vector<line> LineList2;        //第二组边 
+
+extern double x_min;       	//多边形边界 
+extern double x_max;
+extern double y_min;
+extern double y_max; 
+ 
+extern int centerNode_num;     //生成中点的数目 
+extern vector<pos> centerNode;        //生成的中点 
+extern vector<double> evaluation; //代价值 
+
+extern vector<pos> recNode;      	//多边形节点坐标
+extern double precision;   //网格间距
+
+//函数声明
+void ini_variables();   //初始化全局变量 
+void show_ini_info();         //显示初始化信息
+void initialization();        //初始化 
+void ini_LineList1_and_LineList2(); //初始化两组边 
+bool isPointInPloygon(pos, vector<line> );//判断点是否在多边形内
+bool isRayIntersectsSegment(pos, pos, pos);//判断通过一点的水平射线，与线段是否有交点
+double cal_Node_evaluation(pos,vector<line> ,vector<line> );//计算节点的代价值 
+void gen_centerNode();//生成中点的方法
+void dump_result(); //输出结果 
+void dump_debug_file(vector<pos>,string );//输出debug信息
+void ini_precision(); //设置精度 
+void in_mapfile();//输入文件 
+double dist_p2LineList(pos p,vector<line> LineSet);
+double dist_p2Line(pos p,pos s,pos e);
+double dist_p2p(pos p1,pos p2);
+void swap(double &a, double &b);
+void refine_centerNode();
+void rectonly();
+#endif
---
+++ src/rectionly.cpp
@@ -0,0 +1,98 @@
+#include "global.h"
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+struct lin
+{
+    double a;double b;double c;
+};
+
+/* double calcxl(pos a,pos b) */
+/* { */
+/* 	if (fabs(b.x-a.x)<0.00000001) */
+/* 		return 12345.6789; */
+/* 	else	return (b.y-a.y)/(b.x-a.x); */
+/* } */
+
+bool xlequ(double a,double b)
+{
+     if (fabs(a-b)<precision/100) return 1;
+    /* else return 0; */
+     if (fabs(a+2*PI-b)<precision/100) return 1;
+     if (fabs(a-2*PI-b)<precision/100) return 1;
+     return 0;
+}
+double calcxl(pos a,pos b)//base point is b
+{
+    /* double dis=sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y)); */
+    /* double si=dis/(a.y-b.y); */
+    return atan2((a.y-b.y),(a.x-b.x));
+}
+lin twopoint(pos p1,pos p2)
+{
+	lin re;
+    double x1=p1.x,y1=p1.y,x2=p2.x,y2=p2.y;
+	re.a=y1-y2;
+	re.b=x2-x1;
+	re.c=x1*y2-x2*y1;
+	return re;
+}
+
+pos twoline(lin A,lin B)
+{
+	pos re;
+	re.x=(B.b*A.c-A.b*B.c)/(A.b*B.a-A.a*B.b);
+	re.y=(B.a*A.c-A.a*B.c)/(A.a*B.b-A.b*B.a);
+	return re;
+} 
+
+void rectonly()
+{
+    int unsamexl,i,n=centerNode_num;
+    double oldxl,xl;
+    bool samexl=1;
+    lin l1,l2;
+    recNode_num=1;
+    recNode.push_back(centerNode[0]);
+    for (i=1;i<n;i++)
+    {
+        printf("%d %f %f\n",i,xl,oldxl);
+        oldxl=xl;
+        xl=calcxl(centerNode[i],centerNode[i-1]);
+        if (i==1) continue;
+        if (xlequ(oldxl,xl))
+        {
+            if (samexl==0)
+            {
+                if (unsamexl>5)
+                {
+                    l1=twopoint(centerNode[i],centerNode[i-1]);
+                    l2=twopoint(recNode[recNode_num-1],recNode[recNode_num-2]);
+                    recNode_num++;
+                    recNode.push_back(twoline(l1,l2));
+                }
+                recNode_num++;
+                recNode.push_back(centerNode[i-2]);
+            }
+            samexl=1;
+            unsamexl=0;
+        }
+        else
+        {
+            unsamexl++;
+            if (samexl==1)
+            {
+                recNode_num++;
+                recNode.push_back(centerNode[i-1]);
+            }
+            samexl=0;
+        }
+    }
+    recNode_num++;
+    recNode.push_back(centerNode[n-1]);
+    printf("%d\n",recNode_num);
+    for (i=0;i<recNode_num;i++)
+        printf("%f %f\n",recNode[i].x,recNode[i].y);
+}
